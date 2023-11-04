e921a32d5c52ef7a3cc7972587fe997935061cf2
Darndy <102405572+Darndy@users.noreply.github.com>
2023-11-04 02:46:14 -0700

    print out quote with python

---
+++ PYTNPRNT/print1.py
@@ -0,0 +1,4 @@
+print("my second python pogram")
+print('it can also be in a single quote')
+print("print ('this is how you can print a quote with python print')")
+print('print ("this is how you can print a quote with python print")')
