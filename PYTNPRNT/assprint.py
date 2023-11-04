a126367fdc2ac80cdd3226f0c6878fe15cc20cca
Darndy <102405572+Darndy@users.noreply.github.com>
2023-11-04 04:57:57 -0700

    using the print() in many ways

---
+++ PYTNPRNT/assprint.py
@@ -0,0 +1,4 @@
+print("string manipulation execise")
+print('string concatenation is done with "+" sign')
+print('e.g print("hello" + "jenny")')
+print("new lines can be created with a backslash and n")
